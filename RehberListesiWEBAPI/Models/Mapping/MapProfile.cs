2cb86be6f39201e7649554972a3e728091cfef45
oktayefe <oktayefe56@gmail.com>
2022-11-11 23:56:44 +0300

    AutoMapper Eklendi
    Rehber için dto oluşturulup automapper ile kullanıma sunuldu

---
+++ RehberListesiWEBAPI/Models/Mapping/MapProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AutoMapper;
+using RehberListesiWEBAPI.Models.DTO;
+
+namespace RehberListesiWEBAPI.Models.Mapping
+{
+	public class MapProfile : Profile
+	{
+		public MapProfile() => CreateMap<RehberDTO, Rehber>();
+	}
+}
+
