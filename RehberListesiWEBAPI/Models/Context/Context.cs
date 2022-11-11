c724b1f7a454c7cd244562dcde4672f13b4b2891
oktayefe <oktayefe56@gmail.com>
2022-11-11 23:09:01 +0300

    Modellerin Oluşturulması ve Database bağlantısının kurulması
    Modeller oluşturuldu, postgresql veritabanı bağlantısı sağlandı. Ayrıca migration işlemleri yapıldı ve database tabloları oluturuldu

---
+++ RehberListesiWEBAPI/Models/Context/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RehberListesiWEBAPI.Models.Context
+{
+	public class Context : DbContext
+	{
+		
+		public Context(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Rehber> Rehbers { get; set; }
+		public DbSet<IletisimBilgileri> IletisimBilgileris { get; set; }
+	}
+}
+
