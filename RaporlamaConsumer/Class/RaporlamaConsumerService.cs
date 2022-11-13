128a17f2ae7f7e01ed424abf7b502de8c1c4ca8b
oktayefe <oktayefe56@gmail.com>
2022-11-13 13:11:27 +0300

    KAFKA entegrasyonu
    Kafka message queue oluşumu sağlandı fakat haberleşme sağlanamadı. Kafka mesajı gönderilmete fakat mesaj alınamamaktadır. Bu sebepten dolayı kafka tam çalışmıyor.

---
+++ RaporlamaConsumer/Class/RaporlamaConsumerService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+using RaporlamaConsumer.Models.Entity;
+
+namespace RaporlamaConsumer.Class
+{
+	public class RaporlamaConsumerService : IHostedService
+	{
+		private readonly string topic = "Raporlama";
+		private readonly string groupId = "Raporlama_Group";
+		private readonly string bootstrapServers = "localhost:9092";
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			var config = new ConsumerConfig
+			{
+				BootstrapServers = bootstrapServers,
+				GroupId = groupId,
+				AutoOffsetReset = AutoOffsetReset.Earliest
+			};
+			try
+			{
+				 
+				using(var consumerBuilder = new ConsumerBuilder<Ignore, string>(config).Build())
+				{
+					consumerBuilder.Subscribe(topic);
+					var cancelToken = new CancellationTokenSource();
+
+					try
+					{
+						while (true)
+						{
+							var consumer = consumerBuilder.Consume(cancelToken.Token);
+							var raporRequest = JsonSerializer.Deserialize<Raporlama>(consumer.Message.Value);
+							 raporRequest = consumer.Message.Value!=null?( JsonSerializer.Deserialize<Raporlama>(consumer.Message.Value)): JsonSerializer.Deserialize<Raporlama>(Stream.Null);
+							//var raporRequest = JsonSerializer.Deserialize<Raporlama>(consumer.Message.Value);
+
+							Debug.WriteLine($"Konum: {raporRequest.Konum} Kişi Sayısı: {raporRequest.KisiSayisi} Telefon Numarası Sayısı: {raporRequest.TelefonNumarasiSayisi} Raporlama Tarihi: {raporRequest.RaporlamaTalepTarihi.ToString()} ");
+						}
+					}
+					catch (OperationCanceledException)
+					{
+						consumerBuilder.Close();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(ex.Message);
+			}
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
+
--- RehberListesiWEBAPI/Controllers/RaporlamaController.cs
+++ RehberListesiWEBAPI/Controllers/RaporlamaController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RehberListesiWEBAPI.Models.Context;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +19,8 @@ namespace RehberListesiWEBAPI.Controllers
 	public class RaporlamaController : Controller
     {
 		private readonly Context _context;
+		private readonly string bootstrapServers = "localhost:9092";
+		private readonly string topic = "Raporlama";
 
 		public RaporlamaController(Context context)
 		{
@@ -21,18 +28,53 @@ namespace RehberListesiWEBAPI.Controllers
 		}
 
 		[HttpGet]
-		public IActionResult KonumBilgisi()
+		public async Task<IActionResult> KonumBilgisi()
 		{
 			var KonumListesi = _context.IletisimBilgileris.GroupBy(x => x.Konum).Select(x => x.Key).ToList();
 			var sonuc = from item in KonumListesi
 						select new
 						{
 							Konum = item,
-							KisiSayisi = _context.IletisimBilgileris.Where(x => x.Konum == item).Count(),
-							TelefonNumarasi = _context.IletisimBilgileris.Where(x => x.Konum == item && x.TelefonNumarasi != "").Count()
+							KisiSayisi = _context.IletisimBilgileris.Where(x => x.Konum == item).CountAsync(),
+							TelefonNumarasiSayisi = _context.IletisimBilgileris.Where(x => x.Konum == item && x.TelefonNumarasi != "").Count(),
+							RaporlamaTalepTarihi = DateTime.Now.ToString()
 						};
-			return Ok(sonuc);
+			string message = JsonSerializer.Serialize(sonuc);
+			return Ok(await SendOrderRequest(topic,message));
 		}
-    }
+
+		private async Task<bool> SendOrderRequest
+		(string topic, string message)
+		{
+			ProducerConfig config = new ProducerConfig
+			{
+				BootstrapServers = bootstrapServers,
+				ClientId = Dns.GetHostName()
+			};
+
+			try
+			{
+				using (var producer = new ProducerBuilder
+				<Null, string>(config).Build())
+				{
+					var result = await producer.ProduceAsync
+					(topic, new Message<Null, string>
+					{
+						Value = message
+					});
+
+					Debug.WriteLine($"Gönderilen Zaman:{result.Timestamp.UtcDateTime.AddHours(3)}");
+					return await Task.FromResult(true);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Hata Mesajı: {ex.Message}");
+			}
+
+			return await Task.FromResult(false);
+		}
+
+	}
 }
 
